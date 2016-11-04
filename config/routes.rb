b519ca32e20e42ad963ab20cb523f7cefc750300
Iggy <igoririanto@rocketmail.com>
2016-11-04 13:39:27 -0700

    first repo

---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  resources :restaurants
+
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  root 'restaurants#index' #doesn't work with restaurants/index. Seems like root needs to have Controller#Action ('restaurants#new' works; 'restaurants' doesn't work)
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
